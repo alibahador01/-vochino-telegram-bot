db6696d1ef4e87c7e75815078fd2e60eca1f2eb4
alibahador01 <Vochinobot01@gmail.com>
2026-07-30 11:37:15 +0330

    Add PostgreSQL connection and user retrieval functions

--- db.js
+++ db.js
@@ -1,1 +1,28 @@
-..
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+async function getUser(telegramId) {
+  const res = await pool.query('SELECT * FROM users WHERE telegram_id = $1', [String(telegramId)]);
+  return res.rows[0] || null;
+}
+
+async function getUserCards(telegramId) {
+  const user = await getUser(telegramId);
+  const extraRes = await pool.query('SELECT * FROM cards WHERE telegram_id = $1', [String(telegramId)]);
+  const list = [];
+  if (user && user.card_number) {
+    list.push({ card_number: user.card_number });
+  }
+  extraRes.rows.forEach(function (c) { list.push({ card_number: c.card_number }); });
+  return list;
+}
+
+module.exports = {
+  pool,
+  getUser,
+  getUserCards
+};
