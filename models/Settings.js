d5439732e500b7eecbede0fcfcda556e08cb3f9f
alibahador01 <Vochinobot01@gmail.com>
2026-08-10 17:32:14 +0330

    Create Settings.js

---
+++ models/Settings.js
@@ -0,0 +1,24 @@
+// models/Settings.js
+const mongoose = require('mongoose');
+
+const settingsSchema = new mongoose.Schema({
+  key: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  value: {
+    type: mongoose.Schema.Types.Mixed,
+    required: true
+  },
+  description: {
+    type: String,
+    default: ''
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Settings', settingsSchema);
